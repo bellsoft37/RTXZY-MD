22fd612558b07def0139e4dcd273962c42658c8e
dbzzbot <anjaydib3@gmail.com>
2026-03-07 00:36:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.packname = "Made With"; // watermark stikcker packname
 global.author = "Bot Dibzz"; // watermark stikcker author
 global.maxwarn = "5"; // Peringatan maksimum Warn
 
-global.autobio = false; // Set true/false untuk mengaktifkan atau mematikan autobio (default: false)
+global.autobio = true; // Set true/false untuk mengaktifkan atau mematikan autobio (default: false)
 global.antiporn = false; // Set true/false untuk Auto delete pesan porno (bot harus admin) (default: false)
 global.spam = false; // Set true/false untuk anti spam (default: false)
 global.gcspam = false; // Set true/false untuk menutup grup ketika spam (default: false)
